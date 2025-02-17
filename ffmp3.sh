04ab1ef44aed0cacf366fd7022db4e9e89613964
majia365 <104328306+majia365@users.noreply.github.com>
2025-02-17 19:19:51 +0800

    edit ffmp3.sh, pushbin.sh

--- ffmp3.sh
+++ ffmp3.sh
@@ -2,38 +2,38 @@
 
 #音乐音质
 best_quality="-ar 48000 -ac 2 -q:a 0"
-low_quality="-ar 44100 -ac 2 -b:a 128k"
-medium_quality="-ar 44100 -ac 2 -b:a 192k"
 high_quality="-ar 44100 -ac 2 -b:a 320k"
+medium_quality="-ar 44100 -ac 2 -b:a 192k"
+low_quality="-ar 44100 -ac 2 -b:a 128k"
 #广播音质
-am_quality="-ar 11025 -ac 1"
 fm_quality="-ar 22050 -ac 1"
+am_quality="-ar 11025 -ac 1"
+
+default_quality=${best_quality}
 
 #命令行帮助
 print_usage() {
     echo "usage: ffmp3.sh [quality]"
-    echo "\t quality: best/low/medium/high/am/fm"
+    echo "\t quality: best/high/medium/low/fm/am"
     exit
 }
 
 #检查命令行参数
-if [[ $# -eq 0 ]];
-then
-	quality=${best_quality}
-elif [[ $# -eq 1 ]];
-then
+if [[ $# -eq 0 ]]; then
+	quality=${default_quality}
+elif [[ $# -eq 1 ]]; then
     if [[ ${1} == "best" ]]; then
     	quality=${best_quality}
-    elif [[ ${1} == "low" ]]; then
-    	quality=${low_quality}
-    elif [[ ${1} == "medium" ]]; then
-    	quality=${medium_quality}
     elif [[ ${1} == "high" ]]; then
     	quality=${high_quality}
-    elif [[ ${1} == "am" ]]; then
-    	quality=${am_quality}
+    elif [[ ${1} == "medium" ]]; then
+    	quality=${medium_quality}
+    elif [[ ${1} == "low" ]]; then
+    	quality=${low_quality}
     elif [[ ${1} == "fm" ]]; then
     	quality=${fm_quality}
+    elif [[ ${1} == "am" ]]; then
+    	quality=${am_quality}
     else
     	print_usage
     fi
@@ -44,13 +44,12 @@ fi
 #处理.mp4生成.mp3
 for file in *.mp4;
 do
-	if [ "${file}" = "*.mp4" ]
-	then
+	if [[ "${file}" = "*.mp4" ]]; then
+		echo "Not Found mp4."
 		break
 	fi
-	echo "${file}"
-	if [ ! -f "${file%.mp4}.mp3" ]
-	then
+	echo "${file} ..."
+	if [[ ! -f "${file%.mp4}.mp3" ]]; then
 		ffmpeg -loglevel error -i "${file}" -map_metadata -1 -vn ${quality} "${file%.mp4}.mp3"
 	else
 		echo "pass"
--- pushbin.sh
+++ pushbin.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
 scripts=("ffbili.py" "ffconcat.sh" "ffmp3.sh")
+destpath="/Users/zhong/bin"
 
-for script in "${scripts[@]}"
+for script in "${scripts[@]}";
 do
-  echo ${script} "..."
-  cp -f ${script} ~/bin && chmod +x ~/bin/${script}
+    echo "${script} ..."
+    if [[ ! -f "${destpath}/${script}" ]]; then
+        cp ${script} ${destpath} && chmod +x ${destpath}/${script}
+        echo "copy ok"
+    else
+        cmp -s ${script} ${destpath}/${script} && echo "pass" && continue
+        cp -f ${script} ${destpath} && chmod +x ${destpath}/${script}
+        echo "overwrite ok"
+    fi
 done
+
 
